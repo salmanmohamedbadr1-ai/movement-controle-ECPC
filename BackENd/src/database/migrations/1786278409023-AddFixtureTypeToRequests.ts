febf731d7f5898df1b93fb1842c68d038861e629
Nizar Esmat <nizaresmat2000@gmail.com>
2026-08-12 06:16:23 +0300

    Fix enum type naming and column name for fixture_type in requests table

--- BackENd/src/database/migrations/1786278409023-AddFixtureTypeToRequests.ts
+++ BackENd/src/database/migrations/1786278409023-AddFixtureTypeToRequests.ts
@@ -1,21 +1,23 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class AddFixtureTypeToRequests1786278409023
-  implements MigrationInterface
-{
+export class AddFixtureTypeToRequests1786278409023 implements MigrationInterface {
   name = 'AddFixtureTypeToRequests1786278409023';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TYPE "public"."requests_fixturetype_enum" AS ENUM('URINAL', 'TOILET')`,
+      `CREATE TYPE "public"."requests_fixture_type_enum" AS ENUM('URINAL', 'TOILET')`,
     );
+
     await queryRunner.query(
-      `ALTER TABLE "requests" ADD "fixtureType" "public"."requests_fixturetype_enum"`,
+      `ALTER TABLE "requests" ADD "fixture_type" "public"."requests_fixture_type_enum"`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "requests" DROP COLUMN "fixtureType"`);
-    await queryRunner.query(`DROP TYPE "public"."requests_fixturetype_enum"`);
+    await queryRunner.query(
+      `ALTER TABLE "requests" DROP COLUMN "fixture_type"`,
+    );
+
+    await queryRunner.query(`DROP TYPE "public"."requests_fixture_type_enum"`);
   }
 }
